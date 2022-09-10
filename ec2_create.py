10285a7bfae47692b7304fceafecae66eb9cc95c
alexrf45 <fonalex45@gmail.com>
2022-09-10 10:52:25 -0400

    linting for ec2_create complete

--- ec2_create.py
+++ ec2_create.py
@@ -1,11 +1,15 @@
-#!/usr/bin/env python3
+"""
+A simple script for creating an ec2 instance with a security group and key pair
+"""
 
+#!/usr/bin/env python3
+from time import sleep # allows script execution to pause as AWS CLI execution
+                       # on AWS servers are not always instant.
 import boto3
 import boto3auth
 from botocore.exceptions import ClientError
-from time import sleep #allows script execution to pause as AWS CLI execution on AWS servers are not always instant. 
-import os
-import subprocess
+
+
 
 REGION = 'us-east-1'
 AMI_IMAGE_ID = 'ami-07d02ee1eeb0c996c'
@@ -25,37 +29,52 @@ sh get-docker.sh
 sudo usermod -aG docker $USER
 . ~/.bashrc
 '''
-min_count = 1 #Minimum number of ec2 instances created
-max_count = 1 #Maximum number of ec2 instances created
+MIN_COUNT = 1 #Minimum number of ec2 instances created
+MAX_COUNT = 1 #Maximum number of ec2 instances created
 
 
 ## This function creates a keypair and saves it in the current directory as a .pem file
 def create_key_pair():
+    """
+    This function creates a keypair and associates it with the AWS account.
+    """
     ec2_client = boto3.resource("ec2", region_name="us-east-1")
-    outfile = open('ec2demo.pem', 'w')
+    outfile = open('ec2demo.pem', 'w',encoding='UTF-8')
     key_pair = ec2_client.create_key_pair(
         KeyName='ec2-demo',
         KeyType='ed25519',
-        TagSpecifications=[{'ResourceType': 'key-pair','Tags': [{'Key': 'Name','Value': 'ec2-demo'},]},] )
-        # print({key_pair.key_material})
+        TagSpecifications=[
+            {'ResourceType': 'key-pair',
+            'Tags': [
+                {'Key': 'Name','Value': 'ec2-demo'},
+                ]
+            },
+        ]
+    )
     outfile.write(f"{key_pair.key_material}")
-    # Consider adding a print KeyName statement and return it as variable that can be used in the launch_ec2_instance()
+    # Consider adding a print KeyName statement and return
+    # it as variable that can be used in the launch_ec2_instance()
 
 create_key_pair()
 
-#Reference: https://www.edureka.co/community/87793/create-a-security-group-and-rules-using-boto3-module
+#Reference:
+# https://www.edureka.co/community/87793/create-a-security-group-and-rules-using-boto3-module
 
-def ec2_security_group(): #creates a default security group for ssh access
+def ec2_security_group():
+    """
+    This function creates a default security group for ssh access
+    """
     ec2_client = boto3.client("ec2", region_name="us-east-1")
     response = ec2_client.describe_vpcs()
-    vpc_id = response.get('Vpcs', [{}])[1].get('VpcId', '') #queries second vpc created for your acct in the us-east-1 region
+    vpc_id = response.get('Vpcs', [{}])[1].get('VpcId', '')
+    #queries second vpc created for your acct in the us-east-1 region
     try:
         response = ec2_client.create_security_group( #creates the security group
             GroupName='EC2_DEMO',
             Description='SSH Access',
             VpcId=vpc_id)
         security_group_id = response['GroupId']
-        print('Security Group Created %s in vpc %s.' % (security_group_id, vpc_id)) 
+        print(f'Security Group Created {security_group_id} in vpc {vpc_id}.' )
 
         response = ec2_client.authorize_security_group_ingress( #creates ssh rule
             GroupId=security_group_id,
@@ -65,55 +84,65 @@ def ec2_security_group(): #creates a default security group for ssh access
                 'ToPort': 22,
                 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
             ])
-        print('Ingress Successfully Set %s')  
-    except ClientError as e:
-        print(e)
+        print('Ingress Successfully Set %s')
+    except ClientError as client_error:
+        print(client_error)
     return security_group_id
 
-def create_ec2_resource(): #Connects to the ec2 boto3 client with Demo IAM User. 
-                           #Change EC2-Admin to your AWS profile name
- 
-    print("Attempting to create ec2 resource on region: %s" % REGION)
- 
+def create_ec2_resource():
+    """
+    Connects to the ec2 boto3 client with Demo IAM User.
+    Change EC2-Admin to your AWS profile name
+    """
+    print(f'Attempting to create ec2 resource on region: {REGION}')
+
     session = boto3auth.Boto3Auth()
     ec2 = session.auth('ec2')
- 
+
     if ec2 is None:
         raise ConnectionError("Could not create ec2 resource!")
- 
+
     return ec2
- 
- 
+
+
 def assign_tags_to_instance(ec2, instance_id):
+    """
+    Assigns tags to the ec2 instance.
+    """
     print("Waiting for instance to be ready ...")
     sleep(7)
     print("Assigning tags to instance " + instance_id)
- 
+
     ec2.create_tags(Resources=[instance_id], Tags=[{'Key': 'Name', 'Value': NAME},
                                                    {'Key': 'Owner', 'Value': OWNER},
                                                    {'Key': 'RunId', 'Value': RUNID}])
     print("Tags assigned to instance successfully!")
- 
- 
+
+
 def assign_tags_to_volume(instance):
- 
+    """
+    Assigns tags to the volumes created for the ec2 instance.
+    """
+
     volumes = instance.volumes.all()
- 
+
     print("Waiting for volume to be attached ...")
     sleep(7)
     for volume in volumes:
-        print("Assigning tags to volume %s" % (volume.id))
+        print(f'Assigning tags to volume {volume.id}')
         volume.create_tags(Tags=[{'Key': 'Name', 'Value': NAME},
                                  {'Key': 'Owner', 'Value': OWNER},
                                  {'Key': 'RunId', 'Value': RUNID}])
         print("Tags applied to volume successfully!")
- 
- 
+
 def launch_ec2_instance():
- 
+    """
+    Creates the EC2 instance using all the info generated by the previous functions
+    """
+
     ec2 = create_ec2_resource()
- 
-    blockDeviceMappings = [
+
+    block_device_mappings = [
         {
             'DeviceName': DEVICE_NAME,
             'Ebs': {
@@ -126,7 +155,7 @@ def launch_ec2_instance():
 
     # Create Elastic/Public IP for instance
     if PUBLIC_IP:
-        networkInterfaces = [
+        network_interfaces = [
             {
                 'DeviceIndex': 0,
                 'Groups': [ec2_security_group()],
@@ -135,22 +164,24 @@ def launch_ec2_instance():
             }, ]
         instance = ec2.create_instances(ImageId=AMI_IMAGE_ID,
                                         InstanceType=INSTANCE_TYPE,
-                                        NetworkInterfaces=networkInterfaces,
+                                        network_interfaces=network_interfaces,
                                         UserData=USERDATA_SCRIPT,
-                                        MinCount=min_count, MaxCount=max_count,
+                                        MinCount=MIN_COUNT, MaxCount=MAX_COUNT,
                                         KeyName=KEY_PAIR_NAME,
-                                        BlockDeviceMappings=blockDeviceMappings)
+                                        block_device_mappings=block_device_mappings)
     else:
         instance = ec2.create_instances(ImageId=AMI_IMAGE_ID,
                                         InstanceType=INSTANCE_TYPE,
                                         SecurityGroupIds=[ec2_security_group()],
                                         UserData=USERDATA_SCRIPT,
-                                        MinCount=min_count, MaxCount=max_count,
+                                        MinCount=MIN_COUNT, MaxCount=MAX_COUNT,
                                         KeyName=KEY_PAIR_NAME,
-                                        BlockDeviceMappings=blockDeviceMappings)
+                                        block_device_mappings=block_device_mappings)
     if instance is None:
-        raise Exception("Failed to create instance! Check the AWS console to verify creation or try again")
- 
+        raise Exception("""
+        Failed to create instance! Check the AWS console to verify creation or try again
+        """)
+
     print(f'''
     |-----------------------------------------------------------|
     |                                                           |
