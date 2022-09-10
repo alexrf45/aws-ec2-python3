5363d1509e05dec4dea018a18b2b48b1fb08a7dd
alexrf45 <fonalex45@gmail.com>
2022-09-10 09:10:35 -0400

    refactor ec2_delete.py, grouped function calls together, added pprint and .__doc__

--- ec2-delete.py
+++ ec2-delete.py
@@ -1,31 +1,57 @@
 import boto3
+import pprint
 import os
 from time import sleep
 
+
 instance_id = os.environ["EC2_ID"]
+
 def terminate_instance(instance_id):
+    """
+    
+    Function terminates the instance using the EC2_ID variable exported
+    from ec2-create.py. 
+    To-do: 
+        add boto3auth for standardization
+    
+    """
     ec2_client = boto3.client("ec2", region_name="us-east-1")
     response = ec2_client.terminate_instances(InstanceIds=[instance_id])
-    print(response)
-
-terminate_instance(instance_id)
+    pprint.pp(response)
+    
 
 def delete_key_pair():
+    """
+     Function deletes the key pair from ec2-create.py. 
+    To-do: 
+        automate deletion of ssh key - risky
+    
+    """
     ec2_client = boto3.client("ec2", region_name="us-east-1")
     response = ec2_client.delete_key_pair(
         KeyName='ec2-demo'
     )
-    print(response)
+    pprint.pp(response)
 
-delete_key_pair()
-
-sleep(60)
 
 def delete_security_group():
+    """
+    Function deletes the security group from ec2-create.py
+    """
     ec2_client = boto3.client("ec2", region_name="us-east-1")
     response = ec2_client.delete_security_group(
         GroupName='EC2_DEMO'
     )
-    print(response)
+    pprint.pp(response)
 
+
+print(terminate_instance.__doc__)
+terminate_instance(instance_id)
+
+print(delete_key_pair.__doc__)
+delete_key_pair()
+
+sleep(60)
+
+print(delete_security_group.__doc__)
 delete_security_group()
